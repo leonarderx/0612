cac192e3e23d1e6025b1656dad149080e3296786
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-12-06 18:50:15 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include "setting.h"
+using namespace sf;
+
+int main()
+{
+	// Объект, который, собственно, является главным окном приложения
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+	window.setFramerateLimit(FPS);
+	//создание объектов игры
+
+	//ракетки
+	RectangleShape leftBat, rightBat;
+	leftBat.setSize(batSize);
+	rightBat.setSize(batSize);
+	leftBat.setFillColor(leftBatColor);
+	rightBat.setFillColor(rightBatColor);
+	leftBat.setPosition(batOffset, (WINDOW_HEIGHT - batHeight) / 2);
+	rightBat.setPosition(WINDOW_WIDTH - batOffset, (WINDOW_HEIGHT - batHeight) / 2);
+	float leftBatSpeedY = 0.f;
+	float rightBatSpeedY = 0.f;
+
+	//шарик
+	CircleShape ball(ballRadius);
+	ball.setFillColor(ballColor);
+	ball.setPosition((WINDOW_WIDTH - 2 * ballRadius) / 2,
+		(WINDOW_HEIGHT - 2 * ballRadius) / 2);
+	float ball_dx = 2.f;
+	float ball_dy = 2.f;
+	float arr_speed[]{ -5.f, -4.f, -3.f, -2.f, -1.f, 1.f, 3.f, 4.f, 5.f };
+	int index = rand() % 10;
+	float ball_speedx = arr_speed[index];
+	index = rand() % 10;
+	float ball_speedy = arr_speed[index];
+
+	// Главный цикл приложения. Выполняется, пока открыто окно
+	while (window.isOpen())
+	{
+		//1 Обрабатываем очередь событий в цикле
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Пользователь нажал на «крестик» и хочет закрыть окно?
+			if (event.type == Event::Closed)
+				// тогда закрываем его
+				window.close();
+		}
+
+		//2 Обновление объектов
+		//шарик
+		ball.move(ball_speedx, ball_speedy);
+		if (ball.getPosition().x <= 0 || ball.getPosition().x >=
+			(WINDOW_WIDTH - 2 * ballRadius))
+		{
+			ball_speedx = -ball_speedx;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::W)) {
+			leftBatSpeedY = -batSpeed;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S)) {
+			leftBatSpeedY = batSpeed;
+		}
+		leftBat.move(0, leftBatSpeedY);
+		leftBatSpeedY = 0;
+
+		if (ball.getPosition().x <= 0 || ball.getPosition().x >=
+			(WINDOW_WIDTH - 2 * ballRadius))
+		{
+			ball_speedy = -ball_speedy;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::T)) {
+			rightBatSpeedY = -batSpeed;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::G)) {
+			rightBatSpeedY = batSpeed;
+		}
+		rightBat.move(0, rightBatSpeedY);
+		rightBatSpeedY = 0;
+
+	
+		
+		//3 Отрисовка окна
+		//3.1 Очистка окна
+		window.clear();
+		//3.2 Отрисовка объектов (В ПАМЯТИ!)
+		window.draw(leftBat);
+		window.draw(rightBat);
+		window.draw(ball);
+		//3.3 вывод на экран
+		window.display();
+	}
+
+	return 0;
+}
